fa0f3efa9a3304a99a48f75a9b1555361eedb713
Nicolai Andersen <n.andersen758@gmail.com>
2018-02-21 16:15:25 +0100

    FakeRoulette tilføjet

---
+++ RouletteGame.Legacy/FakeRoulette.cs
@@ -0,0 +1,32 @@
+﻿namespace RouletteGame.Legacy
+{
+    public class FakeRoulette : IRoulette
+    {
+        public Field _result
+        {
+            set => _result = value;
+            get => GetResult();
+        }
+
+        public bool SpinFuntionWasCalled { get; set; }
+        public bool GetResultFuntionWasCalled { get; set; }
+
+        public FakeRoulette()
+        {
+            SpinFuntionWasCalled = false;
+            GetResultFuntionWasCalled = false;
+        }
+
+        public void Spin()
+        {
+            _result = new Field(1, Field.Red);
+            SpinFuntionWasCalled = true;
+        }
+
+        public Field GetResult()
+        {
+            GetResultFuntionWasCalled = true;
+            return _result;
+        }
+    }
+}
